6e8ff6604043416cbc2e40d9b38e834a04e8ae75
iiiuooyoo <iiiuooyoo@gmail.com>
2025-03-17 17:55:37 +0900

    feat : lombok 설치 및 log 추가

--- src/main/java/com/learn_spring_boot/quickstart/controller/QuickController.java
+++ src/main/java/com/learn_spring_boot/quickstart/controller/QuickController.java
@@ -1,17 +1,21 @@
 package com.learn_spring_boot.quickstart.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class QuickController {
     @GetMapping("/dummy")
     public String dummy() {
+        log.info("dummy");
         return "dummy";
     }
 
     @GetMapping("/dummy2")
     public String dummy2() {
+        log.info("dummy2");
         return "dummy2";
     }
 }
