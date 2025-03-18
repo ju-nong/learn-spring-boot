57ef297934c28bb4b0599528ce3c2a04eee10e29
iiiuooyoo <iiiuooyoo@gmail.com>
2025-03-18 13:48:49 +0900

    feat : query와 path 파라미터를 갖는 API 추가

--- src/main/java/com/learn_spring_boot/quickstart/controller/QuickController.java
+++ src/main/java/com/learn_spring_boot/quickstart/controller/QuickController.java
@@ -2,6 +2,8 @@ package com.learn_spring_boot.quickstart.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,5 +19,20 @@ public class QuickController {
     public String dummy2() {
         log.info("dummy2");
         return "dummy2";
+    }
+
+    @GetMapping("/member")  // /member?empNo=2&year=2024
+    public String getMember(@RequestParam("empNo") String empNo, @RequestParam("year") int year) {
+        log.info("empNo: {}", empNo);
+        log.info("year: {}", year);
+
+        return "ok";
+    }
+
+    @GetMapping("/company/{id}")    // /company/2234
+    public String getCompany(@PathVariable("id") String id) {
+        log.info("id: {}", id);
+
+        return "ok";
     }
 }
